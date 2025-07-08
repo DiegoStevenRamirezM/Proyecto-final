2fcdc99090e546163a6925687166c344f75c0b64
DiegoStevenRamirezM <diegosteven.ramirez@udea.edu.co>
2025-07-07 23:59:58 -0500

    Declaracion y Implementación inicial de la clase base Personaje: atributos de vida, estado y método de daño

--- personaje.cpp
+++ personaje.cpp
@@ -1,3 +1,29 @@
 #include "personaje.h"
 
-Personaje::Personaje() {}
+Personaje::Personaje(int vidaInicial)
+    : vida(vidaInicial), frameActual(0), anchoFrame(40), altoFrame(40), totalFrames(1)
+{
+    timer = new QTimer(this);
+    muerto = false;
+
+}
+
+Personaje::~Personaje() {}
+
+void Personaje::recibirDanio(int cantidad)
+{
+    if (muerto) return;  // No seguir recibiendo daño si ya está muerto
+
+    vida -= cantidad;
+    if (vida <= 0) {
+        vida = 0;
+        muerto = true;  //  marcar que está muerto
+        return;
+    }
+    mostrarAnimacionDanio();
+}
+
+
+int Personaje::getVida() const { return vida; }
+
+void Personaje::mostrarAnimacionDanio() {}
--- personaje.h
+++ personaje.h
@@ -1,10 +1,38 @@
 #ifndef PERSONAJE_H
 #define PERSONAJE_H
 
-class Personaje
+#include <QObject>
+#include <QGraphicsPixmapItem>
+#include <QPixmap>
+#include <QTimer>
+
+class Personaje : public QObject, public QGraphicsPixmapItem
 {
+    Q_OBJECT
+
 public:
-    Personaje();
+    Personaje(int vidaInicial = 100);
+    virtual ~Personaje();
+
+    virtual void recibirDanio(int cantidad);
+    int getVida() const;
+    bool estaMuerto() const { return muerto; }
+
+protected:
+    QPixmap hojaSprite;
+    QTimer* timer;
+
+    int vida;
+    int frameActual;
+    int anchoFrame;
+    int altoFrame;
+    int totalFrames;
+    bool muerto;
+
+
+
+    virtual void siguienteFrame() = 0;
+    virtual void mostrarAnimacionDanio();
 };
 
 #endif // PERSONAJE_H
