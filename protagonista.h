316fa66592cd8d64636939c051eb58dd5da28416
DiegoStevenRamirezM <diegosteven.ramirez@udea.edu.co>
2025-07-08 16:55:19 -0500

    Declaración de la clase Protagonista y métodos clave para el Nivel 1

--- protagonista.h
+++ protagonista.h
@@ -1,12 +1,48 @@
 #ifndef PROTAGONISTA_H
 #define PROTAGONISTA_H
 
-#include <personaje.h>
+#include "personaje.h"
+#include "enemigo.h"
+#include <QTimer>
 
 class Protagonista : public Personaje
 {
+    Q_OBJECT
+
 public:
     Protagonista();
+    void moverDerecha();
+    void moverIzquierda();
+    void saltar();
+    void detenerse();
+    void mostrarMuerte();
+    void permitirAvanceCompleto(bool valor);
+    void lanzarKamehameha();
+    void animarKamehameha();
+    bool getEstaOcupado() const { return estaOcupado; }
+
+protected:
+    void mostrarAnimacionDanio() override;
+
+private slots:
+    void siguienteFrame();
+    void animarSalto();
+
+private:
+    QTimer *timerSalto;
+    int yFrameFijo;
+    double tiempo, g, v0, velocidadHorizontal;
+    int yInicial, direccionSalto;
+    bool enElAire;
+    bool limiteDerechoActivo;
+
+    enum Estado { Corriendo, Saltando, Quieto };
+    Estado estado;
+
+    // Atributo necesario para el sprite del kamehameha
+    QPixmap spriteKamehameha;
+    //variable para que no se hagan multiples golpes a la vez
+    bool estaOcupado = false;
 };
 
 #endif // PROTAGONISTA_H
