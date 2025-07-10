32a91b12c7468566b7be552a807ad909f48668f4
DiegoStevenRamirezM <diegosteven.ramirez@udea.edu.co>
2025-07-10 15:42:20 -0500

    Declaración de la clase Nivel: gestión de la escena, barra de vida y señales del juego

--- nivel.h
+++ nivel.h
@@ -1,10 +1,42 @@
 #ifndef NIVEL_H
 #define NIVEL_H
 
-class Nivel
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QKeyEvent>
+#include <QProgressBar>
+#include <QLabel>
+#include <QHBoxLayout>
+#include <QWidget>
+#include <QGraphicsPixmapItem>
+#include <QTimer>
+#include "protagonista.h"
+
+class Nivel : public QGraphicsView
 {
+    Q_OBJECT
+
 public:
-    Nivel();
+    explicit Nivel(QWidget *parent = nullptr);
+    void actualizarBarraVida();
+    void mostrarVictoria();
+    void mostrarDerrota();
+
+signals:
+    void nivelCompletado();
+    void gokuMurio();
+
+protected:
+    QGraphicsScene* scene;
+    Protagonista* goku;
+    QProgressBar* barraVida;
+    QLabel* textoVida;
+    QLabel* iconoVida;
+    QWidget* widgetVida;
+    QHBoxLayout* layoutVida;
+    QProgressBar* barraProgreso = nullptr;
+
+    void inicializarEscena(const QString& fondoPath);
 };
 
 #endif // NIVEL_H
