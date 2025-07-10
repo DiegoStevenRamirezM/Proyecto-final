6967284a82005885fbd975de275337eefbfe2a5b
DiegoStevenRamirezM <diegosteven.ramirez@udea.edu.co>
2025-07-09 21:41:17 -0500

    Declaración de la clase Pared como trampa con aceleración

--- pared.h
+++ pared.h
@@ -1,12 +1,22 @@
 #ifndef PARED_H
 #define PARED_H
 
-#include <trampa.h>
+#include "trampa.h"
 
 class Pared : public Trampa
 {
+    Q_OBJECT
+
 public:
-    Pared();
+    Pared(QGraphicsItem* parent = nullptr);
+    void actualizar() override;
+    void recibirImpacto(); // nuevo método para retroceso
+
+private:
+    double tiempo;
+    double aceleracion;
+    double velocidadInicial;
+    double posicionXInicial;
 };
 
 #endif // PARED_H
