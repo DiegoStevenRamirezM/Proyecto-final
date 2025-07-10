c77fb4817f234a8d74f2fa9a73785330945bbb8b
DiegoStevenRamirezM <diegosteven.ramirez@udea.edu.co>
2025-07-09 21:18:06 -0500

    Implementacióon de la clase Trampa: logica de activacion, daño y temporizador de daño continuo

--- trampa.cpp
+++ trampa.cpp
@@ -1,3 +1,48 @@
 #include "trampa.h"
 
-Trampa::Trampa() {}
+Trampa::Trampa(int dano)
+{
+    this->danio = dano;
+    this->ancho = 80;
+    this->alto = 80;
+    this->activa = true;
+    this->timer = nullptr;
+    this->timerDanio = nullptr;
+}
+
+Trampa::~Trampa() {}
+
+int Trampa::getDanio() const {
+    return danio;
+}
+
+bool Trampa::estaActiva() const {
+    return activa;
+}
+
+void Trampa::desactivar() {
+    activa = false;
+    detenerDanioContinuo();
+}
+
+QTimer* Trampa::getTimer() const {
+    return timer;
+}
+
+void Trampa::iniciarDanioContinuo(QObject* receptor, std::function<void()> callback)
+{
+    if (!timerDanio) {
+        timerDanio = new QTimer(receptor);
+        QObject::connect(timerDanio, &QTimer::timeout, receptor, callback);
+        timerDanio->start(800); // cada 800 ms
+    }
+}
+
+void Trampa::detenerDanioContinuo()
+{
+    if (timerDanio) {
+        timerDanio->stop();
+        delete timerDanio;
+        timerDanio = nullptr;
+    }
+}
