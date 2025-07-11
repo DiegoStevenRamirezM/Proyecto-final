e7ad2c52a4758b73699a099a2bf14c412834e0de
DiegoStevenRamirezM <diegosteven.ramirez@udea.edu.co>
2025-07-10 22:19:46 -0500

    Declaracion clase Nivel1View  con todo lo necesario para las trampas y las dos habitaciones

--- nivel1view.h
+++ nivel1view.h
@@ -1,12 +1,57 @@
 #ifndef NIVEL1VIEW_H
 #define NIVEL1VIEW_H
 
-#include <nivel.h>
+#include "nivel.h"
+#include <QTimer>
+#include <QList>
+#include <QSet>
+#include <QProgressBar>
+#include "trampafija.h"
+#include "flecha.h"
+#include "pared.h"
 
 class Nivel1View : public Nivel
 {
+    Q_OBJECT
+
 public:
-    Nivel1View();
+    explicit Nivel1View(QWidget *parent = nullptr);
+
+
+private slots:
+    void verificarColisiones();
+
+private:
+    // Trampas
+    QTimer *temporizadorTrampas;
+    QTimer *verificadorColisiones;
+    QList<Trampa*> trampasActivas;
+    QSet<Trampa*> trampasEnContacto;
+
+    // Desbloqueo de habitación
+    int trampasGeneradas;
+    bool habitacionDesbloqueada;
+    QProgressBar* barraProgreso;
+    QGraphicsTextItem* mensajeDesbloqueo;
+
+    // Transición y desafíos
+    QTimer *verificadorSalida;
+    bool paredCreada;
+    Pared* pared;
+
+    // Desafío de resistencia
+    QTimer* temporizadorResistencia;
+    int segundosRestantes;
+    QGraphicsTextItem* contadorTiempo;
+
+    // Métodos internos
+    void generarTrampa();
+    void eliminarTrampa(Trampa* trampa);
+    void iniciarDesafioPared();
+
+protected:
+    void keyPressEvent(QKeyEvent *event) override;
+    void keyReleaseEvent(QKeyEvent *event) override;
 };
 
 #endif // NIVEL1VIEW_H
