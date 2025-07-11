ccc66f9847a344427d0ae27ba672c89682a71e0f
DiegoStevenRamirezM <diegosteven.ramirez@udea.edu.co>
2025-07-10 20:12:19 -0500

    Declaración de la clase Flecha: trampa móvil con trayectoria oscilante

--- flecha.h
+++ flecha.h
@@ -1,12 +1,24 @@
 #ifndef FLECHA_H
 #define FLECHA_H
 
-#include <trampa.h>
+#include "trampa.h"
+#include <QGraphicsScene>
 
 class Flecha : public Trampa
 {
+    Q_OBJECT
+
 public:
-    Flecha();
+    Flecha(bool haciaDerecha, QGraphicsItem* parent = nullptr);
+    void actualizar() override;
+
+private:
+    bool direccion;  // true = derecha, false = izquierda
+    int velocidad;
+    double amplitud;
+    double frecuencia;
+    int tiempo;
+    int yBase;
 };
 
 #endif // FLECHA_H
