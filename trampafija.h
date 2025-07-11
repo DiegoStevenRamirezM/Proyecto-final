5085040b5ad20955ebfd6ce5240ce54a8374fd77
DiegoStevenRamirezM <diegosteven.ramirez@udea.edu.co>
2025-07-10 19:56:01 -0500

    Declaración de la clase TrampaFija: animación de trampas fijas como estacas, ácido y trampas de piso

--- trampafija.h
+++ trampafija.h
@@ -1,12 +1,25 @@
 #ifndef TRAMPAFIJA_H
 #define TRAMPAFIJA_H
 
-#include <trampa.h>
+#include "trampa.h"
+#include <QString>
+#include <QPixmap>
 
 class TrampaFija : public Trampa
 {
+    Q_OBJECT
+
 public:
-    TrampaFija();
+    TrampaFija(const QString& tipo);
+    void actualizar() override;
+
+private:
+    QPixmap spriteSheet;
+    int frameActual;
+    int totalFrames;
+    int fila;
+    int anchoSprite;
+    int altoSprite;
 };
 
 #endif // TRAMPAFIJA_H
