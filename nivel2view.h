5ba9df7e0b9ce173fa5c246de44761dfaf6ea2ad
DiegoStevenRamirezM <diegosteven.ramirez@udea.edu.co>
2025-07-12 23:36:25 -0500

    Clase Nivel2View declarada para el combate contra Pilaf

--- nivel2view.h
+++ nivel2view.h
@@ -1,12 +1,24 @@
 #ifndef NIVEL2VIEW_H
 #define NIVEL2VIEW_H
 
-#include <nivel.h>
+#include "nivel.h"
+#include "enemigo.h"
+#include <QProgressBar>
 
 class Nivel2View : public Nivel
 {
+    Q_OBJECT
+
 public:
-    Nivel2View();
+    explicit Nivel2View(QWidget *parent = nullptr);
+    void actualizarBarraEnemigo();
+
+private:
+    Enemigo *pilaf;
+    QProgressBar *barraEnemigo;
+
+protected:
+    void keyPressEvent(QKeyEvent *event) override;
 };
 
 #endif // NIVEL2VIEW_H
