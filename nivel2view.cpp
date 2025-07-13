3d1e133b7028d0c397aad46ef248e68f57f202d1
DiegoStevenRamirezM <diegosteven.ramirez@udea.edu.co>
2025-07-12 23:43:37 -0500

    Implementación de Nivel2View: combate con Pilaf, ataques con misiles y controles del jugador

--- nivel2view.cpp
+++ nivel2view.cpp
@@ -1,3 +1,97 @@
 #include "nivel2view.h"
+#include "enemigo.h"
+#include <QGraphicsPixmapItem>
+#include <QProgressBar>
+#include <QLabel>
+#include <QTimer>
 
-Nivel2View::Nivel2View() {}
+Nivel2View::Nivel2View(QWidget *parent) : Nivel(parent)
+{
+    // Fondo
+    inicializarEscena(":/imagenes/Fondo_nivel2.png");
+
+    // Ajustar sprite de Goku
+    goku->setScale(2);
+    goku->setY(goku->y() - 110);
+
+
+    // Crear a Pilaf
+    pilaf = new Enemigo();
+    pilaf->setPos(0, 450);
+    pilaf->setZValue(1);
+    scene->addItem(pilaf);
+    pilaf->setObjetivos(goku, this);
+
+    goku->setEnemigo(pilaf);
+
+    // Barra de vida de Pilaf
+    barraEnemigo = new QProgressBar(this);
+    barraEnemigo->setRange(0, 100);
+    barraEnemigo->setValue(100);
+    barraEnemigo->setFormat("Pilaf: %p%");
+    barraEnemigo->setGeometry(580, 10, 200, 25);
+    barraEnemigo->setStyleSheet(
+        "QProgressBar { border: 2px solid black; border-radius: 5px; background-color: white; }"
+        "QProgressBar::chunk { background-color: blue; }"
+        );
+    barraEnemigo->raise();
+
+    // Temporizador para ataque de Pilaf
+    QTimer* ataquePilaf = new QTimer(this);
+    connect(ataquePilaf, &QTimer::timeout, this, [=]() {
+        if (goku->getVida() <= 0) {
+            ataquePilaf->stop();
+            goku->mostrarMuerte();
+
+            QTimer::singleShot(800, this, [=]() {
+                mostrarDerrota();
+            });
+            return; // evita que siga lanzando misiles
+        }
+
+        if (!pilaf->estaMuerto()) {
+            pilaf->lanzarMisiles(scene, goku);
+        } else {
+            ataquePilaf->stop();
+            mostrarVictoria();
+        }
+
+
+    });
+    ataquePilaf->start(5000);
+}
+
+void Nivel2View::keyPressEvent(QKeyEvent *event)
+{
+    if (goku->estaMuerto() || goku->getEstaOcupado()) return;
+
+    switch (event->key()) {
+    case Qt::Key_C:
+        goku->curarse();
+        actualizarBarraVida();
+        break;
+
+    case Qt::Key_F:
+        goku->animarPunio();
+        break;
+
+    case Qt::Key_G:
+        goku->animarPatada();
+        break;
+
+    case Qt::Key_H:
+        goku->animarBolaEnergia();
+        break;
+
+    default:
+        break;
+    }
+}
+
+void Nivel2View::actualizarBarraEnemigo()
+{
+    if (!pilaf) return;
+
+    int vidaActual = pilaf->getVida();
+    barraEnemigo->setValue(vidaActual);
+}
