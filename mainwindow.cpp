b02e99579123f2be919cf402404e801fd9c5dd9f
DiegoStevenRamirezM <diegosteven.ramirez@udea.edu.co>
2025-07-13 23:26:03 -0500

    Corrección del mainwindow: uso de QStackedWidget para transiciones estables entre menú y niveles

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,21 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QPushButton>
 #include <QApplication>
-
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     setFixedSize(800, 600);
 
-    menuWidget = new QWidget(this);
+    // Crear el stackedWidget
+    stackedWidget = new QStackedWidget(this);
+    setCentralWidget(stackedWidget);
 
+    // Crear menú principal
+    menuWidget = new QWidget();
     QLabel *fondo = new QLabel(menuWidget);
     fondo->setPixmap(QPixmap(":/imagenes/Fondo_menu.png").scaled(800, 600));
     fondo->setFixedSize(800, 600);
@@ -27,55 +33,66 @@ MainWindow::MainWindow(QWidget *parent)
     layout->addWidget(btnNivel1);
     layout->addWidget(btnNivel2);
     layout->addWidget(btnSalir);
-
     menuWidget->setLayout(layout);
-    setCentralWidget(menuWidget);
 
     QString estilo = "font-size: 18px; padding: 10px; background-color: orange; border-radius: 10px;";
     btnNivel1->setStyleSheet(estilo);
     btnNivel2->setStyleSheet(estilo);
     btnSalir->setStyleSheet("font-size: 18px; padding: 10px; background-color: red; color: white; border-radius: 10px;");
 
+    // Agregar solo el menú inicialmente
+    stackedWidget->addWidget(menuWidget);
+    stackedWidget->setCurrentWidget(menuWidget);
+
+    // Inicializar punteros a nullptr
+    nivel1 = nullptr;
+    nivel2 = nullptr;
+
+    // Conectar botones del menú
     connect(btnNivel1, &QPushButton::clicked, this, &MainWindow::iniciarNivel1);
     connect(btnNivel2, &QPushButton::clicked, this, &MainWindow::iniciarNivel2);
     connect(btnSalir, &QPushButton::clicked, this, &QApplication::quit);
 }
 
-MainWindow::~MainWindow() {}
+MainWindow::~MainWindow()
+{
+    delete nivel1;
+    delete nivel2;
+}
+
 void MainWindow::iniciarNivel1()
 {
-    this->hide();  // Oculta el menú
+    if (!nivel1) {
+        nivel1 = new Nivel1View();
+        stackedWidget->addWidget(nivel1);
 
-    Nivel1View *nivel = new Nivel1View();
-    nivel->setAttribute(Qt::WA_DeleteOnClose); // Libera memoria al cerrarse
-    nivel->show();
+        connect(nivel1, &Nivel1View::nivelCompletado, this, &MainWindow::volverAlMenu);
+        connect(nivel1, &Nivel1View::gokuMurio, this, &MainWindow::volverAlMenu);
+    }
 
-    connect(nivel, &Nivel1View::nivelCompletado, this, [=]() {
-        nivel->close();    // Cierra el nivel
-        this->show();      // Muestra de nuevo el menú
-    });
-
-    connect(nivel, &Nivel1View::gokuMurio, this, [=]() {
-        nivel->close();    // Cierra el nivel
-        this->show();      // Vuelve al menú
-    });
+    stackedWidget->setCurrentWidget(nivel1);
 }
 
 void MainWindow::iniciarNivel2()
 {
-    this->hide();
+    if (!nivel2) {
+        nivel2 = new Nivel2View();
+        stackedWidget->addWidget(nivel2);
 
-    Nivel2View *nivel = new Nivel2View();
-    nivel->setAttribute(Qt::WA_DeleteOnClose);
-    nivel->show();
+        connect(nivel2, &Nivel2View::nivelCompletado, this, &MainWindow::volverAlMenu);
+        connect(nivel2, &Nivel2View::gokuMurio, this, &MainWindow::volverAlMenu);
+    }
 
-    connect(nivel, &Nivel2View::nivelCompletado, this, [=]() {
-        nivel->close();
-        this->show();
-    });
+    stackedWidget->setCurrentWidget(nivel2);
+}
 
-    connect(nivel, &Nivel2View::gokuMurio, this, [=]() {
-        nivel->close();
-        this->show();
-    });
+void MainWindow::volverAlMenu()
+{
+    if (stackedWidget->currentWidget() == nivel1 && nivel1)
+        nivel1->detenerNivel();
+
+    if (stackedWidget->currentWidget() == nivel2 && nivel2)
+        nivel2->detenerNivel();
+
+    stackedWidget->setCurrentWidget(menuWidget);
 }
